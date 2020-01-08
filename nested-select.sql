131cac07d3792ab1453124b9cd20d37b55404ea3
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-07 17:56:59 -0800

    countries population show as percentage of Germany's population

--- nested-select.sql
+++ nested-select.sql
@@ -26,3 +26,10 @@ WHERE population >
 AND population <
 (SELECT population FROM world WHERE name = 'Poland')
 
+--5 Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+-- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany'), 0), %)
+FROM world
+WHERE continent = 'Europe'
+
